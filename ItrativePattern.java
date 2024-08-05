ed1ee980b5e37be43727a88cbf3bd1e107efcf3d
dipeshrathod07 <rathoddipesh221@gmail.com>
2024-08-05 21:30:40 +0530

    space patterns

--- ItrativePattern.java
+++ ItrativePattern.java
@@ -172,13 +172,109 @@ public class ItrativePattern {
             11 12 13 14 15
          */
     }
+
+    static void pattern5()
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j = 1; j<=5; j++)
+            {
+                if(i<= j)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        /*
+            *****
+             ****
+              ***
+               **
+                *
+         */
+        for(int i=1;i<=5;i++)
+        {
+            for(int j = 1; j<=5; j++)
+            {
+                if(i+j > 5)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+         
+        /*
+             *
+            **
+           ***
+          ****
+         *****
+         */
+    }
+    
+    static void pattern6()
+    {
+         for(int i = 1 ; i <= 5; i++)
+         {
+            for(int j = 5 ; j>i ; j--)
+            {
+                    System.out.print(" ");
+            }
+            for(int k = 1; k <= (2*i-1);k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+
+         }
+
+         for(int i = 4 ; i > 0 ; i--)
+         {
+            for(int j = 5; j>i;j--)
+            {
+                System.out.print(" ");
+            }
+            for(int k = 1 ; k <=(2*i-1); k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+         }
+
+         /*
+             
+                       *
+                      ***               
+                     *****
+                    *******
+                   *********
+                    *******
+                     *****
+                      ***
+                       *
+
+          */
+    }
     
     public static void main(String[] args) {
 
        // pattern1();
        // pattern2();
         //  pattern3();
-        pattern4();
+        //pattern4();
+
+        //pattern5();
+        pattern6();
         
     }
 }
