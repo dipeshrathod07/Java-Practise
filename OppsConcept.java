cfd214a17edf1ead481ef8c7cc82f11552478121
dipeshrathod07 <rathoddipesh221@gmail.com>
2024-08-07 12:11:46 +0530

    accessModifiers

--- OppsConcept.java
+++ OppsConcept.java
@@ -1,4 +1,30 @@
+
+class Testing{
+
+    private void show()
+    {
+        System.out.println("Hi");
+    }
+    public void show1()
+    {
+        
+        System.out.println("Hello");
+    }
+    protected void show2()
+    {
+        System.out.println("holla");
+    }
+
+}
+
 public class OppsConcept {
 
+    public static void main(String[] args) {
+        
+        Testing t = new Testing();
+       
+        t.show1();
+        t.show2();
+    }
     
 }
